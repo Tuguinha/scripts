64c6b6bb551e5569771a8fa83f0e3a6961304ac3
Sara Mendes <sara.ferraz.mendes@gmail.com>
2016-06-07 11:09:59 +0200

    addedlatlon

--- dnacalc.py
+++ dnacalc.py
@@ -13,15 +13,40 @@ Seqlength = float( len( DNAseq ))
 
 print( 'Length is ' + str(Seqlength) )
 
+
+#for loop
+
+TotalStrong = 0
+TotalWeak = 0
+
+Bases = "CGTA"
+for Base in Bases:
+	Count = DNAseq.count( Base )
+	Frequency = DNAseq.count( Base ) / Seqlength
+	print ( '{}: {:.2f}'.format(Base, Frequency ) )
+	if Base in 'GC': 
+		TotalStrong = TotalStrong + Count
+	elif Base in 'TA': 
+		TotalWeak = TotalWeak + Count
+
+		print ( TotalStrong, TotalWeak )
+
+
 NumberA = DNAseq.count('A')
 NumberT = DNAseq.count('T')
 NumberG = DNAseq.count('G')
 NumberC = DNAseq.count('C')
 
+
+#before
+
 print('A: {:.2f}'.format( NumberA / Seqlength ))
 print('T: {:.2f}'.format( NumberT / Seqlength ))
 print('C: {:.2f}'.format( NumberC / Seqlength ))
 print('G: {:.2f}'.format( NumberG / Seqlength ))
+
+
+#melting temp
 
 TotalStrong = NumberG + NumberC
 TotalWeak = NumberA + NumberT
